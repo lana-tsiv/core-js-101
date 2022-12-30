6ac4df8ea2012171a5998f237fda5007f994c3d6
Lana <tiushkievich00@mail.ru>
2022-12-30 22:57:19 +0100

    feat: add solution for the 2.4th task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -69,7 +69,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  Math.hypot(x2 - x1, y2 - y1);
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
